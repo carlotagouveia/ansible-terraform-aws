f4f1e04e11ea4f01531eecb7b5c73b41b3edd38e
carlota <carlotagouveiaa@hotmail.com>
2024-07-11 17:24:42 +0100

    add security group to tf

--- main.tf
+++ main.tf
@@ -10,3 +10,28 @@ provider "aws" {
     region = "us-east-1"
 }
 
+resource "aws_security_group" "nginx" {
+  name = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
