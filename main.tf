36acee787130d12df74e20a38e10e243895acae3
carlota <carlotagouveiaa@hotmail.com>
2024-07-11 17:16:42 +0100

    create terraform file - local variables

---
+++ main.tf
@@ -0,0 +1,8 @@
+locals {
+    vpc_id = "vpc-0dc7ff5bd56061e73"
+    subnet_id = "subnet-06d1297753a647bf4"
+    ssh_uder = "ubuntu"
+    key_name = "devops"
+    private_key_path = "~/Downloads/devops.pem"
+}
+
